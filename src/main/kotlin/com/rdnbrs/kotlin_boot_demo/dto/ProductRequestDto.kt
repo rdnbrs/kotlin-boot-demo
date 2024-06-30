53ec473a6c7ad62f39a0885d5014deb2ee2bd06c
rdnbrs <barisrdn@gmail.com>
2024-06-30 19:10:48 +0300

    unused import deleted

--- src/main/kotlin/com/rdnbrs/kotlin_boot_demo/dto/ProductRequestDto.kt
+++ src/main/kotlin/com/rdnbrs/kotlin_boot_demo/dto/ProductRequestDto.kt
@@ -1,7 +1,5 @@
 package com.rdnbrs.kotlin_boot_demo.dto
 
-import com.rdnbrs.kotlin_boot_demo.entity.Category
-
 data class ProductRequestDto(
     override var id: Long? = null,
     var name: String,
